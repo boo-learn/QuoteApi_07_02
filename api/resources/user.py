f6099db353d66107e0cddb71300f5558e5868501
boo <booblegum42@gmail.com>
2022-02-08 08:48:05 -0800

    add User

---
+++ api/resources/user.py
@@ -0,0 +1,25 @@
+from api import Resource, reqparse, db
+from api.models.user import UserModel
+from api.schemas.user import user_schema, users_schema
+
+
+class UserResource(Resource):
+    def get(self, user_id=None):
+        user = UserModel.query.get(user_id)
+        if not user:
+            return {"Error": f"User with id={user_id} not found"}, 404
+        return user_schema.dump(user), 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("username", required=True)
+        parser.add_argument("password", required=True)
+        data = parser.parse_args()
+        user = UserModel(**data)
+        db.session.add(user)
+        db.session.commit()
+        return user_schema.dump(user), 201
+
+    # @auth.login_required
+    # def post(self, author_id):
+    #    ...
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from api import api, app
 from api.resources.quote import QuoteResource
 from api.resources.author import AuthorResource
+from api.resources.user import UserResource
 from config import Config
 
 api.add_resource(QuoteResource,
@@ -11,6 +12,9 @@ api.add_resource(QuoteResource,
 api.add_resource(AuthorResource,
                  '/authors/<int:author_id>',
                  '/authors')  # <-- requests
+api.add_resource(UserResource,
+                 '/users/<int:user_id>', # GET
+                 '/users') # POST
 
 if __name__ == '__main__':
     app.run(debug=Config.DEBUG, port=Config.PORT)
